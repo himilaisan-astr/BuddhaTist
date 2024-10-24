1f80f720d3f2bbfa03e6f78f6870464bf589c460
Your Name <vnguyen.the111@gmail.com>
2024-10-24 14:31:26 +0700

    initial update

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,43 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { StyleSheet, Image, Platform } from 'react-native';
+
+import { Collapsible } from '@/components/Collapsible';
+import { ExternalLink } from '@/components/ExternalLink';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { lessons } from '@/lessons';
+
+export default function TabTwoScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
+      headerImage={<Ionicons size={310} name="code-slash" style={styles.headerImage} />}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Explore</ThemedText>
+      </ThemedView>
+      <ThemedText>Đây là một app để giúp mẹ thi đậu Phật cử.</ThemedText>
+      
+      {
+        lessons.map((lesson, index) => (
+          <Collapsible key={lesson.id} title={`Bài ${lesson.id}: ${lesson.title}`}>
+            <ThemedText>{lesson.content}</ThemedText>
+          </Collapsible>
+        ))
+      }
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  headerImage: {
+    color: '#808080',
+    bottom: -90,
+    left: -35,
+    position: 'absolute',
+  },
+  titleContainer: {
+    flexDirection: 'row',
+    gap: 8,
+  },
+});
---
+++ lessons.tsx
@@ -0,0 +1,102 @@
+export const lessons = [
+    {
+        id: 1,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 2,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 3,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 4,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 5,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 6,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 7,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 8,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 9,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 10,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 11,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 12,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 13,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 14,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 15,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 16,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 17,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 18,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 19,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+    {
+        id: 20,
+        title: "ĐẠO PHẬT",
+        content: "1-10"
+    },
+]
